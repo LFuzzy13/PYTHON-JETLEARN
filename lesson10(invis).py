40857f0c077c7ad7f47463a196f087b3f5fd72cd
LFuzzy13 <littlefuzzy26@gmail.com>
2026-07-01 10:47:02 +0100

    Add files via upload

--- lesson10(invis).py
+++ lesson10(invis).py
@@ -4,7 +4,7 @@ import time
 
 print("opencv version :", cv2.__version__)
 
-capture_video = cv2.VideoCapture("INVISIVID2.mp4")
+capture_video = cv2.VideoCapture("INVISIVID3.mp4")
 
 time.sleep(0.03)
 
@@ -40,10 +40,10 @@ while capture_video.isOpened():
     frame_count +=1
     img = np.flip(img,axis=1)
     HSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    lower_red_one = np.array([0, 120, 70])
-    upper_red_one = np.array([10, 255, 255])
-    lower_red_two = np.array([160, 120, 70]) 
-    upper_red_two = np.array([180, 255, 255])
+    lower_red_one = np.array([20, 40, 60])
+    upper_red_one = np.array([40, 255, 255])
+    lower_red_two = np.array([40, 40, 60]) 
+    upper_red_two = np.array([80, 255, 255])
     mask1 = cv2.inRange(HSV,lower_red_one,upper_red_one)
     mask2 = cv2.inRange(HSV,lower_red_two,upper_red_two)
     mask = mask1+mask2
